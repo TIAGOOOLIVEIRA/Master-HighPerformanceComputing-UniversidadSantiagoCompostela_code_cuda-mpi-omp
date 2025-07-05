360957035cad0aa9d7fc01c2bb96a26500885908
Tiago Oliveira <ti.olive@gmail.com>
2025-07-05 12:48:58 +0200

    the solution for rma_01_fence.c

--- advanced_parallelprogramming/assignement_omp_mpi/RMA-MPI_IO_assignment/Lab4/rma_01_fence.c
+++ advanced_parallelprogramming/assignement_omp_mpi/RMA-MPI_IO_assignment/Lab4/rma_01_fence.c
@@ -12,7 +12,7 @@
  *
  * 0 -> 1 -> 2 -> .. -> N -> 0
  * 
- * Compile: mpicc -Wall -O3 -std=c99 -o 01_rma_fence 01_rma_fence.c
+ * Compile: mpicc -Wall -O3 -std=c99 -o rma_01_fence rma_01_fence.c
  * Run: no arguments required
  */
 
@@ -22,45 +22,60 @@
  
  #define A_SIZE 1000
  
- int main(int argc, char ** argv)
+ #define MEC(call) { int res = call; \
+   if (res != MPI_SUCCESS) { \
+     char error_string[MPI_MAX_ERROR_STRING]; \
+     int length; \
+     MPI_Error_string(res, error_string, &length); \
+     fprintf(stderr, "MPI Error: %s\nCall: %s\n", error_string, #call); \
+     MPI_Abort(MPI_COMM_WORLD, res); \
+   } \
+ }
+ 
+ int main(int argc, char **argv)
  {
    int *a, b[2];
    int size, rank;
  
-   MPI_Init(&argc, &argv);
+   MEC(MPI_Init(&argc, &argv));
+   MEC(MPI_Comm_size(MPI_COMM_WORLD, &size));
+   MEC(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
  
-   MPI_Comm_size(MPI_COMM_WORLD, &size);
-   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+   //MPI_Alloc_mem for proper alignment and performance
+   MEC(MPI_Alloc_mem(A_SIZE * sizeof(int), MPI_INFO_NULL, &a));
  
-   /* create private memory */
-   //TODO: Memory should be allocated using MPI_Alloc_mem instead,
-   //      to get the correct alignment
-   a = (int *) malloc(A_SIZE * sizeof(int));
+   a[0] = a[1] = rank;
  
-   a[0]=a[1]=rank; /* use private memory as usual */
+   //memory in "a" as remotely accessible using MPI_Win_create
+   MPI_Win win;
+   MEC(MPI_Win_create(a, A_SIZE * sizeof(int), sizeof(int), MPI_INFO_NULL,
+                      MPI_COMM_WORLD, &win));
  
-   //TODO: Collectively declare memory in "a" as remotely accessible
-   
-   //TODO: Enclose RMA operations with calls to MPI_Win_fence
-   
    int source = (rank + size - 1) % size;
    int target = (rank + 1) % size;
-   b[0] = (rank+1) * 10;
-   b[1] = (rank+1) * 20;
+   b[0] = (rank + 1) * 10;
+   b[1] = (rank + 1) * 20;
  
-   //TODO: Replace these Two-Sided with One-Sided operations
+   //Fence to start RMA
+   MEC(MPI_Win_fence(0, win));
  
-   MPI_Request req;
-   MPI_Isend(b, 2, MPI_INT,
-            target, 0, MPI_COMM_WORLD, &req);
-   MPI_Recv(a, 2, MPI_INT,
-            source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+   //One-sided op replacing two-sided Isend/Recv
+   //puts the two values of b into the start of the next rank's array
+   MEC(MPI_Put(b, 2, MPI_INT, target, 0, 2, MPI_INT, win));
  
-   //TODO: Free MPI Window
-   //TODO: Use MPI_Free_mem instead of free for memory allocated with MPI_Alloc_mem
-   free(a);
-   
-   MPI_Finalize();
+   //Fence to end RMA epoch (ensures put is complete) - like a barrier
+   //a synchronization point for all processes
+   MEC(MPI_Win_fence(0, win));
+ 
+   //Print the values received in a
+   //a is private memory of the process, but we can access it after the fence
+   printf("Rank %d received values in a: %d %d (from rank %d)\n", rank, a[0], a[1], source);
+ 
+   //Free MPI Window and memory
+   MEC(MPI_Win_free(&win));
+   MEC(MPI_Free_mem(a));
+ 
+   MEC(MPI_Finalize());
    return 0;
  }
  
